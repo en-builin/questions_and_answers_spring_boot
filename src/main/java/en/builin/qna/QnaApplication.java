b8d88d843e7e9cc5707dd2e72457befe82dcf7df
Evgeniy Buylin <en.builin@gmail.com>
2022-01-06 16:41:01 +0800

    Layout content template

--- src/main/java/en/builin/qna/QnaApplication.java
+++ src/main/java/en/builin/qna/QnaApplication.java
@@ -15,6 +15,14 @@ public class QnaApplication {
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     * Thymeleaf Layout Dialect Bean https://ultraq.github.io/thymeleaf-layout-dialect/getting-started/
+     */
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(QnaApplication.class, args);
     }
